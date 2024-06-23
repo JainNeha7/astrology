2a27869082958599e207ed9af02db182a999d008
Kauslesh <kausleshjst@gmail.com>
2024-06-23 12:57:52 +0530

    DB Fixes

--- src/dbConfig/dbConfig.js
+++ src/dbConfig/dbConfig.js
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 
 export async function connect() {
     try {
-        mongoose.connect(process.env.MONGO_URI);
+        // mongoose.connect(process.env.MONGO_URI);
+        mongoose.connect("mongodb+srv://kausleshjst:WgnVZPccTnkRM5Zp@cluster0.tv7xeav.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
         const connection = mongoose.connection;
 
         connection.on('connected', () => {
